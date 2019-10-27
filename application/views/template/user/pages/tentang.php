1e99f3cccfe98cba239696d1562f03e3e854ffe2
Khoirul <43804770+khoirulrhojiqin@users.noreply.github.com>
2019-10-27 12:32:33 +0800

    nwe
    new

--- application/views/template/user/pages/tentang.php
+++
@@ -1,6 +0,0 @@
-<br>
-<div class="container">
-<h2><strong><center>Tentang CoopMart Almuawanah</center></strong></h2>
-</div>
-<hr>
-<p><blockquote> Copmart marupakan salah satu unit usaha dari koperasi Al-Muawannah UIN Alauddin Makassar yang bergerak dipenyediaan barang makanan dan kebutuhan pokok sehari-hari.</blockquote></p>
--- assets/front_end/assets/js/prs.js
+++
@@ -1,6 +0,0 @@
-function HapusData(kode,path,modul)
-{
-	if (confirm('Anda yakin mau menghapus data ini?')) {
-		window.location.href = path + modul + '/hapusdata/' + kode;
-	}
-}
